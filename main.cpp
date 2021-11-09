9c59bd609b6b83161f9fc04e79ae5b7b3bae5826
Герман <winami5582@soulsuns.com>
2021-11-09 13:00:06 +0300

    reads and print numbers

--- main.cpp
+++ main.cpp
@@ -4,12 +4,19 @@ using namespace std;
 void number_of_peoducts_div(){
     cout << "Hello world!" << endl;
     /**
-    1. читать и запомнить входные числа
+    +1. читать и запомнить входные числа
     2. вывести все пары
     3. вместо вывода пары посчитать её произведение
     4. вывести только пары, произведение которых кратно 55
     5. только количество пар
     */
+    int n(0)
+    cin>>n;
+    vector<int> numbers;
+    for (int i(0),i<n,i++)
+        cin<<numbers[i];
+    for (int i(0),i<n,i++)
+        cin<< numvers[i]<<"";
 }
 
 
