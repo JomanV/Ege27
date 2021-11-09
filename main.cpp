259609a6f337aebd2326fdc91fe8fbc19b8d0bb6
Герман <winami5582@soulsuns.com>
2021-11-09 13:46:41 +0300

    print pairs

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 void number_of_peoducts_div(){
@@ -9,13 +10,15 @@ void number_of_peoducts_div(){
     4. вывести только пары, произведение которых кратно 55
     5. только количество пар
     */
-    int n(0)
-    cin>>n;
+    int n(0);
+    cin >> n;
     vector<int> numbers(n,0);
-    for (int i(0),i<n,i++)
-        cin<<numbers[i];
-    for (int i(0),i<n,i++)
-        cin<< numvers[i]<<"";
+    for (int i(0);i<n;i++)
+        cin>>numbers[i];
+
+    for (int j(0);j<n-1;j++)
+        for (int i(j+1);i<n;i++)
+            cout << numbers[j]<<" "<< numbers[i]<<endl;
 }
 
 
