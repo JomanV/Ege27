a2eb72fdf68c1752c59fcd5c0833654eb8bd629f
Герман <winami5582@soulsuns.com>
2021-11-16 12:52:17 +0300

    read and print numbers

--- main.cpp
+++ main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-void number_of_peoducts_div(){
-    cout << "Hello world!" << endl;
+void max_sum(){
 
+/*
+1.считать число и сохранить
+2.вывести пары
+3.вместо пар вывести сумму
+4.проверить делимость сумм
+5.вывести наиб сумму
+*/
+    int n(0);
+    cin >> n;
+    vector<int> numbers(n);
+    for (int i(0);i<n;i++)
+        cin >> numbers[i];
+    for (int i(0);i<n;i++)
+        cout << numbers[i] << " ";
 }
 
 
 
 int main()
 {
-
-    cout << "Hello world!" << endl;
-    number_of_peoducts_div();
+    max_sum();
     return 0;
 }
