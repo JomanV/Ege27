edf6489c41205a68a38e8506102eab3db13fbe87
Герман <winami5582@soulsuns.com>
2021-11-16 12:42:00 +0300

    new plan

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
 using namespace std;
+void number_of_peoducts_div(){
+    cout << "Hello world!" << endl;
+
+}
+
+
 
 int main()
 {
+
     cout << "Hello world!" << endl;
+    number_of_peoducts_div();
     return 0;
 }
