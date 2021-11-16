00ff01e7f3acc7f4a9388e0e0326877e8d06b3e5
Герман <winami5582@soulsuns.com>
2021-11-16 14:15:25 +0300

    print max sum pair(not effective)

--- main.cpp
+++ main.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <fstream>
 
 using namespace std;
 void max_sum(){
-    ifstream ifs("data.txt");
 /*
  + 1.считать число и сохранить
  + 2.вывести пары
  + 3.вместо пар вывести сумму
  + 4.проверить делимость сумм
-5.вывести наиб сумму
+  5.вывести наиб сумму
 */
     int n(0);
-    ifs >> n;
+    int maxN(-1001);
+    cin >> n;
     vector<int> numbers(n,0);
     for (int i(0);i<n;i++)
-        ifs>>numbers[i];
+        cin>>numbers[i];
     for (int j(0);j<n-1;j++)
-        for (int i(j+1);i<n;i++)
-            if ((numbers[j] + numbers[i])%2==1)
-            cout <<numbers[j] + numbers[i]<<endl;
+    for (int i(j+1);i<n;i++)
+    {
+        int cur_sum(numbers[j] + numbers[i]);
+        if (cur_sum%2==1 and maxN<cur_sum)
+        maxN=cur_sum;
+    }
+    if (maxN==-1001)
+        maxN=-1;
+    cout <<maxN<<endl;
 }
-
 
 
 int main()
