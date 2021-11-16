44aaf3c168f0b20085d156717f988b6473fd66ef
Герман <winami5582@soulsuns.com>
2021-11-16 13:29:26 +0300

    print pairs

--- main.cpp
+++ main.cpp
@@ -7,18 +7,19 @@ void max_sum(){
     ifstream ifs("data.txt");
 /*
  + 1.считать число и сохранить
-2.вывести пары
+ + 2.вывести пары
 3.вместо пар вывести сумму
 4.проверить делимость сумм
 5.вывести наиб сумму
 */
     int n(0);
     ifs >> n;
-    vector<int> numbers(n);
+    vector<int> numbers(n,0);
     for (int i(0);i<n;i++)
-        ifs >> numbers[i];
-    for (int i(0);i<n;i++)
-        cout << numbers[i] << " ";
+        ifs>>numbers[i];
+    for (int j(0);j<n-1;j++)
+        for (int i(j+1);i<n;i++)
+            cout << numbers[j]<<" "<< numbers[i]<<endl;
 }
 
 
