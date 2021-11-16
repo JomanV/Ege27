21fc2de950512a386eccfb8050554eaa2b154fc0
Герман <winami5582@soulsuns.com>
2021-11-16 12:56:49 +0300

    read file

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 void max_sum(){
-
+    ifstream ifs("data.txt");
 /*
-1.считать число и сохранить
+ + 1.считать число и сохранить
 2.вывести пары
 3.вместо пар вывести сумму
 4.проверить делимость сумм
 5.вывести наиб сумму
 */
     int n(0);
-    cin >> n;
+    ifs >> n;
     vector<int> numbers(n);
     for (int i(0);i<n;i++)
-        cin >> numbers[i];
+        ifs >> numbers[i];
     for (int i(0);i<n;i++)
         cout << numbers[i] << " ";
 }
