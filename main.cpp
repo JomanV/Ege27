c6f7dd8cdae91ebfd1ebfff57c1156c78c57f89e
Герман <winami5582@soulsuns.com>
2021-11-30 12:58:56 +0300

    ineffective code: print min sum of distant pairs

--- main.cpp
+++ main.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 #include <vector>
-#include <fstream>
 
 using namespace std;
 
 void ex(){
-ifstream ifs("data.txt");
 /*
 +1.считать все числа
 +2.вывести все числа
 +3.вывести все пары чисел
 +4.вывести только достаточно далекие пары
 +5.вместо вывода пары - сумма
-6.найти минимальную сумму
++6.найти минимальную сумму
 */
-int n(0);
-ifs>>n;
+int n(0); int min_sum(1000000);
+cin>>n;
 vector<int> num(n,0);
 for (int i(0);i<n;i++)
-    ifs>>num[i];
+    cin>>num[i];
 for (int i(0);i<n;i++)
     for (int j(i+4);j<n;j++)
-        cout<<num[i]+num[j]<<endl;
+        if (num[i]+num[j]<min_sum)
+            min_sum=num[i]+num[j];
+cout<<min_sum<<endl;
 }
 int main()
 {
