4ad5cb0d5d8f95425ebe56169bfed84e100bfea1
Герман <winami5582@soulsuns.com>
2021-11-30 12:46:27 +0300

    read and print

--- main.cpp
+++ main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
-ex(){
+void ex(){
+ifstream ifs("data.txt");
 /*
-1.считать все числа
-2.вывести все числа
++1.считать все числа
++2.вывести все числа
 3.вывести все пары чисел
 4.вывести только достаточно далекие пары
 5.вместо вывода пары - сумма
 6.найти минимальную сумму
 */
+int n(0);
+ifs>>n;
+vector<int> num(n,0);
+for (int i(0);i<n;i++)
+    ifs>>num[i];
+for (int i(0);i<n;i++)
+    cout<<num[i]<<" ";
 }
 int main()
 {
     ex();
+    return 0;
 }
