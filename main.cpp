5ade4a4796fe949dcd51e7c0a0a7afb537183311
Герман <winami5582@soulsuns.com>
2021-11-30 14:00:33 +0300

    read 4 numbers for worm from vector

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 void ex(){
+ifstream ifs("data.txt");
 /*
 +1.считать 4 числа в червяка
-2.число в хвосте червяка-потенциальный минимум зи пройденных
+2.число в хвосте червяка-потенциальный минимум из пройденных
 3.сдвинуть все числа в червяке влево
 4.считать пятоу число в зубы червыку
 5.------------вывести червяка и пройденный минимм для проверки
@@ -14,13 +16,19 @@ void ex(){
 8.один зод чераяка закончен, повторяем
 */
 int n(0);
-cin>>n;
+ifs>>n;
 vector<int> num(n,0);
 vector<int> worm(4,0);
 
-for (int i(0);i<4;i++)
-    cin>>worm[i];
+for (int i(0);i<n;i++)
+    ifs>>num[i];
+for (int i(0);i<n-3;i++){
+    for (int j(0);j<4;j++)
+        worm[j]=num[i+j];
+    cout<<worm[0]<<" "<<worm[1]<<" "<<worm[2]<<" "<<worm[3]<<endl;
+    }
 }
+
 int main()
 {
     ex();
