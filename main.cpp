e50f04b3421f327ca4a845c9a1c190081f4ca468
Joman_V <German.Vlasenko@bk.ru>
2021-12-07 12:03:02 +0300

    print count sum%36==0

--- main.cpp
+++ main.cpp
@@ -9,18 +9,18 @@ void ex(){
 +3.вывод всех сумм
 +4.вывод всех сумм, где каждое число не дальше 9 чисел 
 +5.вывод пар кратных 36
-6.вывод количества сумм кратных 36
++6.вывод количества сумм кратных 36
 */
-int n(0); int sum(0);
+int n(0); int count_sum(0);
 cin>>n;
 vector<int> all(n,0);
 for (int i(0);i<n;i++)
   cin>>all[i];
 for (int i(0);i<n;i++)
   for (int j(i+1);j<n and (j-i)<=9;j++){
-    sum=all[i]+all[j];
-    if (sum%36==0)
-      cout<<sum<<' ';}
+    if ((all[i]+all[j])%36==0)
+      count_sum++;}
+cout<<count_sum<<endl;
 }
 int main()
 {
