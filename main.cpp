900a458c2586075edd9cb011b3e3f74744c395b4
Joman_V <German.Vlasenko@bk.ru>
2021-12-07 11:40:40 +0300

    read and print

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
-ex(){
+void ex(){
 /*
-1.считывание N чисел
++1.считывание N чисел
 2.вывод всех пар
 3.вывод всех сумм
 4.вывод всех сумм, где каждое число не дальше 9 чисел 
 5.вывод количества сумм равных 36
 */
+int n(0);
+cin>>n;
+vector<int> all(n,0);
+for (int i(0);i<n;i++)
+  cin>>all[i];
+for (int i(0);i<n;i++)
+  cout<<all[i]<<' ';
 }
 int main()
 {
