18227ba6bdd3b8a4708905a87c9ff1b12a23654e
Joman_V <German.Vlasenko@bk.ru>
2021-12-12 17:15:34 +0300

    read and print

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 ex() {
     /*
-    1.считать числа
+    +1.считать числа
     2.находить максимальное число
     3.находить второе по величине максимальное число
     4.вывести сумму первых максимальных чисел
     */
+    int n(0)
+        cin >> n;
+    vector<int> numbers;
+    for (int i(0), i < n, i++)
+        cin << numbers[i];
+    for (int i(0), i < n, i++)
+        cin << numvers[i] << "";
 }
 int main()
 {
