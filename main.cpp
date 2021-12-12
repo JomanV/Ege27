bf14e74502b52fab1064dbe89b01eb79e0fe71ab
Joman_V <German.Vlasenko@bk.ru>
2021-12-12 15:26:17 +0300

    add development plan

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
 using namespace std;
+void number_of_products_div() {
+    /*
+    1. читать входные числа
+    2. отсортировать числа по кратности 15, 5, 3 и остальные
+    3. вывести каждую группу
+    4. вывести количество пар кратных 15
+    */
+}
+
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    number_of_products_div();
     return 0;
 }
