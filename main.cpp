a80dc744a55a20a560320a7c63a8724951df42f5
Joman_V <German.Vlasenko@bk.ru>
2021-12-12 15:39:14 +0300

    read and print

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 void number_of_products_div() {
     /*
-    1. читать входные числа
+    1. читать и выводить входные числа
     2. отсортировать числа по кратности 15, 5, 3 и остальные
     3. вывести каждую группу
     4. вывести количество пар кратных 15
     */
+    int n(0);
+    cin >> n;
+    vector<int> numbers(n, 0);
+    for (int i(0); i < n; i++)
+        cin >> numbers[i];
+    for (int i(0); i < n; i++)
+        cout << numbers[i] << " ";
 }
 
 
@@ -15,5 +23,4 @@ void number_of_products_div() {
 int main()
 {
     number_of_products_div();
-    return 0;
 }
