5446132d37d24ac5dd468ceca94b921c58db0885
Joman_V <German.Vlasenko@bk.ru>
2021-12-12 17:21:50 +0300

    print max number

--- main.cpp
+++ main.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
-
-ex() {
+void ex() {
     /*
     +1.считать числа
-    2.находить максимальное число
+    +2.находить максимальное число
     3.находить второе по величине максимальное число
     4.вывести сумму первых максимальных чисел
     */
-    int n(0)
-        cin >> n;
-    vector<int> numbers;
-    for (int i(0), i < n, i++)
-        cin << numbers[i];
-    for (int i(0), i < n, i++)
-        cin << numvers[i] << "";
+    int n(0); int max(-1001); int num(0);
+    cin >> n;
+    for (int i(0); i < n; i++) {
+        cin >> num;
+        if (num > max)
+            max = num;
+    }
+    cout << max;
 }
 int main()
 {
