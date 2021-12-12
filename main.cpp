b9eba114a34fb284eba5232272cfcebe624aa2ec
Joman_V <German.Vlasenko@bk.ru>
2021-12-12 16:39:35 +0300

    read and print

--- main.cpp
+++ main.cpp
@@ -4,11 +4,18 @@ using namespace std;
 void number_of_products_div() {
     /*
     «адание #2
-    1. читать входные числа
+    +1. читать входные числа
     2. отсортировать числа по кратности 154, 7, 2, 11 и остальные
     3. вывести каждую группу
     4. вывести количество пар кратных 154
     */
+    int n(0);
+    cin >> n;
+    vector<int> numbers(n, 0);
+    for (int i(0); i < n; i++)
+        cin >> numbers[i];
+    for (int i(0); i < n; i++)
+        cout << numbers[i] << " ";
 }
 
 
@@ -16,5 +23,4 @@ void number_of_products_div() {
 int main()
 {
     number_of_products_div();
-    return 0;
 }
