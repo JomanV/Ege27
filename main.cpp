80d3aeacb69b1523f226b8222ae0b0d51cad0b22
Герман <winami5582@soulsuns.com>
2021-12-21 14:12:03 +0300

    print sort array which doesnt have duplicate numbers

--- main.cpp
+++ main.cpp
@@ -8,19 +8,26 @@ void ex(){
 /*
 +1.ввод всех данных,
 +2.добавление служебных переменных для решения задачи,
-3.вычисление ответа по этим служебным переменным.
++3.вычисление ответа по этим служебным переменным.
 */
-int n(0);
+int n(0); int a(0);
 cin>>n;
 vector<int> num(n,0);
-vector<int> a(3,-1);
-for (int i(0); i<n; i++)
-    cin>>num[i];
+vector<int> num_1(n,0);
+vector<int> b(3,-1);
+for (int i(0); i<n; i++){
+    cin>>a;
+    num[i]=a;
+    num_1[i]=a;}
+for (int i(0); i<n-1; i++)
+    for (int j(i+1); j<n; j++){
+     if (num_1[i]==num_1[j])
+        num[j]=-1;}
 
 sort(num.begin(),num.end());
 
 for (int i(0); i<n; i++)
-    cout<<num[i]<<' ';5
+    cout<<num[i]<<' ';
 }
 int main()
 {
