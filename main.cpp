3f3cd48212387ec57a06433aaa58259a76c269a8
Герман <winami5582@soulsuns.com>
2021-12-21 12:38:43 +0300

    read and print

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void ex(){
 /*
-1.ввод чисел,
-2.вывод чисел на экран,
++1.ввод чисел,
++2.вывод чисел на экран,
 3.вывод пар, вывод троек,
 4.вывод только подходящих троек,
 5.подсчёт подходящих троек
 */
+int n(0);
+cin>>n;
+vector<int> num(n,0);
+for (int i(0); i<n; i++)
+    cin>>num[i];
+for (int i(0); i<n; i++)
+    cout<<num[i]<<' ';
 }
 int main()
 {
