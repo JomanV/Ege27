0731c05b4c45cc59705f697baa4554569170d7b6
Герман <winami5582@soulsuns.com>
2021-12-21 14:05:59 +0300

    add extra services

--- main.cpp
+++ main.cpp
@@ -7,16 +7,20 @@ using namespace std;
 void ex(){
 /*
 +1.ввод всех данных,
-2.добавление служебных переменных для решения задачи,
++2.добавление служебных переменных для решения задачи,
 3.вычисление ответа по этим служебным переменным.
 */
 int n(0);
 cin>>n;
 vector<int> num(n,0);
+vector<int> a(3,-1);
 for (int i(0); i<n; i++)
     cin>>num[i];
+
+sort(num.begin(),num.end());
+
 for (int i(0); i<n; i++)
-    cout<<num[i]<<' ';
+    cout<<num[i]<<' ';5
 }
 int main()
 {
