7ed9ef2fb83475e31d8f14dc15f04020e9464233
Герман <winami5582@soulsuns.com>
2021-12-21 17:00:39 +0300

    print all min numers, which comes out of the worm

--- main.cpp
+++ main.cpp
@@ -7,22 +7,28 @@ void ex(){
 /*
 +1.ввод чисел
 +2.создать червячка, которвый двикается вправо по массиву
-3.вывод минимальное число из всех выходящих из червяка чисел
++3.вывод минимальное число из всех выходящих из червяка чисел
 4.вывод минимального числа и входящего в голову червячка, каждый раз как идет вправо
 5.вывод минимальной суммы из всех пар
 */
-int n(0); int a(0); vector<int> worm(4,0);
+int n(0); int a(1001); vector<int> worm(4,0);
 cin>>n;
-cin>>worm[0]
-cin>>worm[1]
-cin>>worm[2]
-cin>>worm[3]
+vector<int> min_n(n-4,0);
+cin>>worm[0];
+cin>>worm[1];
+cin>>worm[2];
+cin>>worm[3];
 for (int i(4);i<n;i++){
-    worm[0]=worm[1]
-    worm[1]=worm[2]
-    worm[2]=worm[3]
+    if (a>worm[0])
+        a=worm[0];
+    worm[0]=worm[1];
+    worm[1]=worm[2];
+    worm[2]=worm[3];
     cin>>worm[3];
+    min_n[i-4]=a;
     }
+for (int i(0);i<n-4;i++)
+    cout<<min_n[i]<<" ";
 }
 int main()
 {
