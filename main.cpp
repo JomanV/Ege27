b462b30e0bb8e22e0a61a03d78c4302f7a36869e
Герман <winami5582@soulsuns.com>
2021-12-21 13:54:45 +0300

    read and print

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 void ex(){
 /*
-1.ввод всех данных,
++1.ввод всех данных,
 2.добавление служебных переменных для решения задачи,
 3.вычисление ответа по этим служебным переменным.
 */
+int n(0);
+cin>>n;
+vector<int> num(n,0);
+for (int i(0); i<n; i++)
+    cin>>num[i];
+for (int i(0); i<n; i++)
+    cout<<num[i]<<' ';
 }
 int main()
 {
