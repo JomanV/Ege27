ee3f8b8d1a8825260815711a41964222d458b01d
Joman_V <German.Vlasenko@bk.ru>
2021-12-24 16:14:44 +0300

    create array[2,40]

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void ex(){
+/*
+1.заполнить массив 2 на 40 нулями
+2.затем заполнить массив с счетсчиками [x>40][x%40]+=1
+3.суммитровать комплементарные остатки для 40 отдельно, 20 отдельно, остальные отдельно
+*/
+int a(0); int i(0);
+vector< vector<int> > num(2,vector<int>(40,0));
+
+for (int n(0); n<2; n++){
+		for (int j(0); j<40; j++)
+		cout<<num[n][j]+n*40+j<<" ";
+	cout<<endl;}
+	
+}
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    ex();
 }
+
