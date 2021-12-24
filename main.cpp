491845046d4656a1216f1eb3aca4267a8b9cf8ac
Joman_V <German.Vlasenko@bk.ru>
2021-12-24 17:17:44 +0300

    read and print

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void ex(){
 /*
-1.считать и запомнить числа
++1.считать и запомнить числа
 2.вывести все пары чисел
 3.вместо пар вывести произведения
 4.проверить делимость произведений
 5.посчитать и вывести количество подходящих пар
 */
+int n(0);
+cin>>n;
+vector<int> num(n,0);
+for (int i(0); i<n; i++)
+    cin>>num[i];
+for (int i(0); i<n; i++)
+    cout<<num[i]<<' ';
 }
 int main()
 {
